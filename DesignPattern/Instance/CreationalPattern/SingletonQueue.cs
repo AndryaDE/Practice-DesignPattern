cb9d0f3469cc7ddb6b495568f0b3bf50b6f8391f
Andrya <43105402+AndryaDE@users.noreply.github.com>
2020-08-12 14:16:54 +0200

    Singleton Queue added

---
+++ DesignPattern/Instance/CreationalPattern/SingletonQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instance.CreationalPattern.SingletonQueue {
+    class SingletonQueue {
+
+        internal static ConcurrentQueue<SingletonQueue> queue = new ConcurrentQueue<SingletonQueue>();
+
+        public string content = "";
+
+        public SingletonQueue(string msg) {
+            content = msg;
+            queue.Enqueue( this );
+        }
+
+
+    }
+
+    //---------------------------------------------
+
+    static class Program {
+        static internal void Run() {
+            var text1 = new SingletonQueue("Erster");
+            var text2 = new SingletonQueue("Zweiter!");
+            var text3 = new SingletonQueue("Kettenunterbrecher!");
+
+            while(SingletonQueue.queue.TryDequeue(out SingletonQueue item)) {
+                Console.WriteLine(item.content);
+            }
+        }
+    }
+
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using Instance.CreationalPattern.Pool;
 using Instance.BehavioralPattern.State;
+using Instance.CreationalPattern.SingletonQueue;
 
 namespace DesignPattern {
     class Program {
         static void Main(string[] args) {
 
 
-            Console.WriteLine("asd");
+            Console.WriteLine("Hello World");
 
             //Instance.BehavioralPattern.State.Program.Run(); //StateMachine with Questions as States
             //Instance.BehavioralPattern.StateStack.Program.Run(); //StateMachine with a StateStack of Questions
-            Instance.CreationalPattern.Pool.Program.Run(); //StateMachine with a StateStack of Questions
+            //Instance.CreationalPattern.Pool.Program.Run();
+            Instance.CreationalPattern.SingletonQueue.Program.Run();
 
-            //Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
